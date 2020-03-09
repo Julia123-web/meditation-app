535031d02185739049ce39957a4aa9cba23cfdb2
Julia123-web <juliadesouzaesilva@gmail.com>
2020-03-09 18:00:56 +0100

    post of quotes working

--- Quotes/router.js
+++ Quotes/router.js
@@ -1,16 +1,29 @@
 const { Router } = require("express");
 const Quotes = require("./model");
-const auth = require("../auth/middleWare");
+// const auth = require("../auth/middleWare");
 
 const router = new Router();
 
-router.get("/quotes", auth, async function(request, response, next) {
+router.get("/quotes", async function(request, response, next) {
   try {
-    const videos = await Quotes.findAll();
-    response.send(videos);
+    const quotes = await Quotes.findAll();
+    response.send(quotes);
   } catch (error) {
     next(error);
   }
+
+  router.post("/quotes", async function(request, response, next) {
+    try {
+      // console.log("TEST2", request.body);
+      const newQuote = await Quotes.create({
+        quotes: request.body.quotes
+      });
+      console.log("TEST2", newQuote);
+      response.send(newQuote);
+    } catch (error) {
+      next(error);
+    }
+  });
 });
 
 module.exports = router;
